38fc86530035316fd3ad8693a757b335f9e0a6a9
Abhijith Anil Vamadev <36528036+aaabhijith13@users.noreply.github.com>
2023-08-24 15:01:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.sidebar.title("Stock Predictor")
 stock_list = ["AAPL", "GOOGL", "AMZN", "MSFT"]
 
 # Multi-input for stock tickers using multiselect dropdown
-tickers = st.sidebar.multiselect("Select a Stock Ticker to predict: ", stock_list)
+ticker = st.sidebar.select("Select a Stock Ticker to predict: ", stock_list)
 
 # Choice of prediction duration
 options = ["Next Day", "Next 7 Days", "Next 14 Days", "Next 30 Days"]
@@ -17,19 +17,17 @@ choice = st.sidebar.radio("Predict Adjusted Close Prices for: ", options)
 
 # Action button
 if st.sidebar.button("Predict"):
+    # Path for image and CSV
+    image_path = os.path.join("Images", f"{ticker}_predictions.png")
+    csv_path = os.path.join("data","results", f"{ticker}_data.csv")
 
-    for ticker in tickers:
-        # Path for image and CSV
-        image_path = os.path.join("Images", f"predictions_{ticker}.png")
-        csv_path = os.path.join("data", f"{ticker}_data.csv")
+    # Display image
+    if os.path.exists(image_path):
+        st.image(image_path, caption=f"Prediction for {ticker}")
 
-        # Display image
-        if os.path.exists(image_path):
-            st.image(image_path, caption=f"Prediction for {ticker}")
-
-        # Display table data from CSV
-        if os.path.exists(csv_path):
-            data = pd.read_csv(csv_path)
-            st.write(data)
+    # Display table data from CSV
+    if os.path.exists(csv_path):
+        data = pd.read_csv(csv_path)
+        st.write(data)
 
 # To run the app, you would use the command: streamlit run your_script_name.py

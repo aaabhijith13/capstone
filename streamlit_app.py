29f7c3ae5926f49e3c93279f8dcc47fd89387037
Abhijith Anil Vamadev <36528036+aaabhijith13@users.noreply.github.com>
2023-08-24 05:41:04 -0400

    first_commit

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+import boto3
+
+def main():
+    st.markdown("<h1 style='text-align: center; color: white;'>Stock Price Prediction</h1>", unsafe_allow_html=True)
+
+    # Collect input from the user
+    stock = st.selectbox('Select a stock:', ['AAPL', 'TLSA', 'GOOGL'])
+    days = st.slider('Number of days to predict:', 1, 30)
+
+    # When user clicks the 'Predict' button, send data to SageMaker
+    if st.button('Predict'):
+        # Define the SageMaker endpoint URL
+        endpoint_url = "https://your_sagemaker_endpoint_url"
+
+        # Define the payload
+        payload = {
+            "stock": stock,
+            "duration": days
+        }
+        runtime = boto3.client('sagemaker-runtime')
+
+        response = runtime.invoke_endpoint(
+            EndpointName='YourEndpointName',
+            ContentType='application/json',
+            Body='YourInputDataInJSONFormat'
+        )
+
+        print(response['Body'].read())
+
+        # Make the request to the SageMaker endpoint
+        # response = requests.post(endpoint_url, json=payload)
+        # predictions = response.json().get('predictions', [])
+
+        # Display the predictions as a graph
+        plt.figure(figsize=(10, 6))
+        plt.plot(np.arange(len(predictions)), predictions, '-o')
+        plt.title(f"Predicted Prices for {stock} over {days} Days")
+        plt.xlabel("Days")
+        plt.ylabel("Predicted Price")
+        st.pyplot(plt)
+
+if __name__ == "__main__":
+    main()

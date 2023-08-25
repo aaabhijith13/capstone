1c0b244cecda357050c6b33df6f08b8048aff807
Abhijith Anil Vamadev <36528036+aaabhijith13@users.noreply.github.com>
2023-08-25 02:34:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import yfinance as yf
 import requests
+import matplotlib.pyplot as plt
 
 API_KEY = '0999f084017643be9b8dc053c3a258e3'
 ENDPOINT = 'https://newsapi.org/v2/everything'
@@ -48,18 +49,50 @@ def display_news_scrollable(articles):
         # Display the title for the article, making it clickable
         st.markdown(f"[**{article['title']}**]({article['url']})")
 
+def get_trend(current_price, previous_close):
+    """Return 'up' if stock is trending upwards, 'down' otherwise, and 'unknown' if either value is None."""
+    if current_price is None or previous_close is None:
+        return 'unknown'
+    return 'up' if current_price > previous_close else 'down'
+
 
 # CSS for styling
 st.markdown("""
 <style>
 body {
-    color: #333332;
     font-family: 'Arial', sans-serif;
-    background-color: #400090;
 }
+
+/* Setting the main background to white */
+.css-1aumxhk {
+    background-color: #FFFFFF;
+}
+
+/* Setting the sidebar background to white */
+.css-j075dz, .css-1l02zno {
+    background-color: #FFFFFF;
+}
+
+/* Setting font color to purple */
+.css-901oao, .css-16my406, .css-1n6we3e {
+    color: #800080;
+}
+
 .stButton > button {
-    color: #fff;
-    background-color: #400090;
+    color: #800080;
+    background-color: #FFFFFF;
+    border: 1px solid #800080;
+    transition: background-color 0.3s ease;
+}
+.stButton > button:hover {
+    background-color: #800080;
+    color: #FFFFFF;
+}
+
+/* CSS for scrollable news section */
+.news-section {
+    height: 100px;
+    overflow-y: auto;
 }
 </style>
 """, unsafe_allow_html=True)
@@ -68,52 +101,104 @@ body {
 st.sidebar.title("Stock Predictor")
 
 # Pre-defined list of popular stocks (can be expanded)
-stock_list = ["AAPL", "GOOGL", "AMZN", "MSFT"]
-
-# Input for stock tickers using multiselect dropdown
+stock_list = ["AMZN", "MSFT", "AAPL", "GOOGL"]
 ticker = st.selectbox("Select a Stock Ticker to predict: ", stock_list)
-
-# Choice of prediction duration
-options = ["Next Day", "Next 7 Days", "Next 14 Days", "Next 30 Days"]
+options = ["Next 30 Day", "Next 60 Days", "Next 90 Days", "Next 130 Days"]
 choice = st.sidebar.radio("Predict Adjusted Close Prices for: ", options)
 
 # Action button
 if st.sidebar.button("Predict"):
+    left_name, right_name = st.columns(2)
     stock_info = yf.Ticker(ticker)
-    info = stock_info.info  # Fetching the stock details
+    info = stock_info.info
+    with st.container():
+        st.write(f"<h2 style='text-align: center; font-weight: bold;'>{info['longName']} - {ticker}</h2>", unsafe_allow_html=True)
+        current_price = info.get('regularMarketPrice', None)
+        previous_close = info.get('regularMarketPreviousClose', None)
+        trend = get_trend(current_price, previous_close)
+        trend_symbol = "🔼" if trend == "up" else "🔽"
+    # with right_name:
+    #     with st.container():
+    #         st.write(f"{trend_symbol}")
 
-    # Create two columns: one for news and one for stock details
-    left_col, right_col = st.columns(2)
+    with st.container():  # Container for news and stock stats
+        # Create two columns: one for news and one for stock details
+        left_col, right_col = st.columns([4, 6])
+        # Display news articles in a scrollable panel in the left column
+        with left_col:
+            news_data = get_news(ticker)
+            left_col.markdown(f"<h3 style='text-align: center;'>Recent News for {ticker}</h2>", unsafe_allow_html=True)
 
-    # Display news articles in a scrollable panel in the left column
-    with left_col:
-        news_data = get_news(ticker)
-        left_col.subheader('Recent News')
-        display_news_scrollable(news_data[:6])  # Display the first 6 articles
+            display_news_scrollable(news_data[:3])
 
-    # Display stock details in the right column
-    with right_col:
-        right_col.subheader(f"Statisfor {info['longName']}")
-        right_col.write(f"**{info['longName']} ({ticker})**")  # Display the stock name
-        right_col.write(f"**Current Price:** ${info['regularMarketPrice'] if 'regularMarketPrice' in info else 'N/A'}")
-        right_col.write(f"**Market Cap:** ${info['marketCap'] if 'marketCap' in info else 'N/A'}")
-        right_col.write(f"**52 Week High:** ${info['fiftyTwoWeekHigh'] if 'fiftyTwoWeekHigh' in info else 'N/A'}")
-        right_col.write(f"**52 Week Low:** ${info['fiftyTwoWeekLow'] if 'fiftyTwoWeekLow' in info else 'N/A'}")
+        # Split stock details into left and right sections
+        with right_col:
+            right_col.markdown(f"<h3 style='text-align: center;'>Stats for {ticker}</h2>", unsafe_allow_html=True)
 
-    # Path for image and CSV
-    image_path = os.path.join("Images", f"{ticker}_predictions.png")
-    csv_path = os.path.join("data", "results", f"{ticker}_data.csv")
-    print(csv_path)
+            # Left stats section
+            stats_left, stats_right = right_col.columns(2)
+            with stats_left:
+                stats_left.markdown(f"<em>Volume:</em> {info.get('volume', 'N/A')}", unsafe_allow_html=True)
+                stats_left.markdown(f"<em>Previous Close:</em> {previous_close}", unsafe_allow_html=True)
+                stats_left.markdown(f"<em>Open:</em> ${info.get('regularMarketOpen', 'N/A')}", unsafe_allow_html=True)
+                day_range = f"{info.get('regularMarketDayLow', 'N/A')} - {info.get('regularMarketDayHigh', 'N/A')}"
+                stats_left.markdown(f"<em>Day's Range:</em> {day_range}", unsafe_allow_html=True)
 
-    # Display image
-    if os.path.exists(image_path):
-        st.image(image_path, caption=f"Prediction for {ticker}")
+            # Right stats section
+            with stats_right:
+                stats_right.markdown(f"<em>Market Cap:</em> ${info.get('marketCap', 'N/A')}", unsafe_allow_html=True)
+                stats_right.markdown(f"<em>52 Week High:</em> ${info.get('fiftyTwoWeekHigh', 'N/A')}", unsafe_allow_html=True)
+                stats_right.markdown(f"<em>52 Week Low:</em>${info.get('fiftyTwoWeekLow', 'N/A')}", unsafe_allow_html=True)
+                stats_right.markdown(f"<em>PE Ratio:</em> {info.get('forwardPE', 'N/A')}", unsafe_allow_html=True)
 
-    # Display table data from CSV
+            graph_col1, graph_col2 = st.columns(2)
+
+            past_year = stock_info.history(period="5y")['Close']
+            stats_left.markdown(f"<h3 style='text-align: center;'>Close Prices</h2>", unsafe_allow_html=True)
+            stats_left.line_chart(past_year, height=300)
+
+            # Display Sparkline for the last 5 years' Open data below stats_right
+            past_year_open = stock_info.history(period="5y")['Open']
+            stats_right.markdown(f"<h3 style='text-align: center;'>Open Prices</h2>", unsafe_allow_html=True)
+            stats_right.line_chart(past_year_open, height=300)
+
+
+    st.markdown(f"<h3 style='text-align: center;'>Predictions for {ticker}</h2>", unsafe_allow_html=True)
+
+    csv_path = os.path.join("data", "results", "pred", f"{ticker}_data.csv")
     if os.path.exists(csv_path):
         data = pd.read_csv(csv_path)
-        # print(csv_path)
-        # st.line_chart(data=data, x="Actual", y="Predictions", width=0, height=0, use_container_width=True)
 
-        st.write(data)
+        with st.container():  # Container for graph and table
+            graph_col, table_col = st.columns(2)
+
+            # Plot graph in the left column
+            with graph_col:
+                plt.figure(figsize=(10, 6))
+                if choice == "Next 30 Day":
+                    rows_to_display = data.tail(30)
+                elif choice == "Next 60 Days":
+                    rows_to_display = data.tail(60)
+                elif choice == "Next 90 Days":
+                    rows_to_display = data.tail(90)
+                else:
+                    rows_to_display = data
+            with graph_col:
+                graph_col.line_chart(rows_to_display.set_index('Date')[['Actual', 'Predictions']])
+
+
+            # Display table data in the right column
+            with table_col:
+                table_col.write(rows_to_display)
+
+    with st.container():  # Container for image
+        plt.plot(rows_to_display['Date'], rows_to_display['Predictions'], label='Predictions')
+        plt.plot(rows_to_display['Date'], rows_to_display['Actual'], label='Actual')
+        plt.legend()
+        plt.xticks(rotation=90)
+        plt.title(f'{ticker} Predicted vs Actual Adjusted Close Prices')
+        plt.xlabel("Dates from 2020 to 2023")
+        plt.ylabel("Adjusted Close Prices")
+        graph_col.pyplot(plt)
+
 
